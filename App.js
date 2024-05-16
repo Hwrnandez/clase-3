cad9d38bd042c1f3835e14957e584e4d731db695
Hwrnandez <20220215@ricaldone.edu.sv>
2024-05-15 22:30:26 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+
+/*Importación de las dependencias para crear la navegación */
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
+/* Importación de las pantallas */
+import pantalla1 from './screens/pantalla1';
+ 
+ 
+export default function App() {
+ 
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>  
+      <Stack.Navigator
+        initialRouteName='Pantalla1'
+ 
+        >
+        
+        <Stack.Screen name="Pantalla" component={pantalla1} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ data/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+      id: '1',
+      title: '#002752',
+      src: require('../src/img/morado.jpg'),
+    },
+    {
+      id: '2',
+      title: '#FF6600',
+      src: require('../src/img/amarillo.jpg'),
+    },
+    {
+      id: '3',
+      title: '#7C7B79',
+      src: require('../src/img/azul.jpg'),
+    }
+  ];
+ 
+  export default data;
